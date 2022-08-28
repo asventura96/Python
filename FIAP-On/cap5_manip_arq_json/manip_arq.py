796f32fbb7a0c23c7a90069f37979f59451c021b
André Ventura <asventura@live.com>
2022-08-28 17:11:14 -0300

    Criação do Cap. 5

---
+++ FIAP-On/cap5_manip_arq_json/manip_arq.py
@@ -0,0 +1,9 @@
+#arquivo = open ("Python\/FIAP-On\/cap5_manip_arq_json\/arquivo.txt", "w")
+
+#arquivo.write ("Meu texto1")
+
+#arquivo.close()
+
+with open ("Python\/FIAP-On\/cap5_manip_arq_json\/arquivo.txt", "a") as arquivo:
+    arquivo.write("\nAtum na batata!!!")
+    arquivo.write("\nIsto eh um arquvo!!!")
