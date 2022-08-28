b1d84fe045f0ef57de5b2b5eed4c3c609851da19
André Ventura <asventura@live.com>
2022-08-28 17:56:33 -0300

    Inclusão de aprendizado

---
+++ FIAP-On/cap5_manip_arq_json/textos.py
@@ -0,0 +1,11 @@
+texto = "O menino de pijama listrado"
+        #012345678901234567890123456
+        #765432109876543210987654321 -
+print(texto[0:8])
+print(texto[9:11])
+print(texto[12:])
+print(texto[-15:])
+print(texto[::-1])
+
+# 0:8:2
+# posicao inicial : quantidade de informacao a percorrer : passso
