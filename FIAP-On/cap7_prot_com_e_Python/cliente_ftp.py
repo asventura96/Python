0d7f41868982f4a9aa5ae04bbc80cddad7aade85
André Ventura <asventura@live.com>
2022-10-16 17:42:54 -0300

    Criação de uma Aplicação Cliente FTP

---
+++ FIAP-On/cap7_prot_com_e_Python/cliente_ftp.py
@@ -0,0 +1,17 @@
+from ftplib import * #type: ignore
+
+ftp = FTP('ftp.ibiblio.org')
+print(ftp.getwelcome())
+
+usuario = input("Digite o usuário: ")
+senha = input("Digite a senha: ")
+
+ftp.login(usuario, senha)
+print("Diretório atual de trabalho: ", ftp.pwd())
+
+ftp.cwd('pub')
+print("Diretório corrente: ", ftp.pwd())
+
+print(ftp.retrlines('LIST'))
+
+ftp.quit()
