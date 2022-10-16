b9de61c39da61bb8d102aca25f656a13311bee61
André Ventura <asventura@live.com>
2022-10-16 14:54:51 -0300

    Criação do Cap. 7

---
+++ FIAP-On/cap7_prot_com_e_Python/socket.py
@@ -0,0 +1,8 @@
+import socket
+
+resp = "S"
+while (resp == "S"):
+    url = input("Digite uma url: ")
+    ip = socket.gethostbyname(url)
+    print("O IP referente à url informada é: ", ip)
+    resp = input("Digite <s> para continuar ou qualquer outra tecla para encerrar: ").upper()
